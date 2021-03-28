12875399f31a794043cb0fa32d949b333e3afeb6
Adrian Dyjeciński <adriandyjecinski@gmail.com>
2021-03-28 12:59:15 +0200

    Add mybackend - car api

--- mybackend/index.js
+++ mybackend/index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Redis configuration
 const redis = require('redis');
 
 const redisClient = redis.createClient({
@@ -18,6 +19,7 @@ redisClient.on('connect',  () => {
     console.log("Connected to redis client.");
 });
 
+// Postgres configuration
 const { Pool } = require('pg');
 
 const pgClient = new Pool({
@@ -32,17 +34,57 @@ pgClient.on('error', () => {
     console.log("Postgres not connected");
 });
 
-pgClient
-.query('CREATE TABLE IF NOT EXISTS numbers (number INT)')
-.catch( (err) => {
-    console.log(err);
+pgClient.query('CREATE TABLE IF NOT EXISTS cars (id UUID UNIQUE, brand VARCHAR, color VARCHAR, topSpeed INT, PRIMARY KEY (id))')
+        .catch( (err) => {
+            console.log(err);
+        });
+
+// API
+app.get("/cars", (req, res) => {
+    pgClient.query('SELECT * FROM cars;', (err, result) => {
+        if (err) {
+            console.log(err.stack);
+        } else {
+            const data = result.rows;
+            res.send(data)
+            console.log('Postgres : read cars from database');
+        }
+    });
 });
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
+app.get("/cars/:id", (req, res) => {
+    const id = req.params.id;
+
+    redisClient.exists(id, (err, result) => { 
+        if (result < 1) {
+
+            redisClient.hgetall(id, (err, redis) => {
+                if (err) {
+                    console.log(err.stack)
+                } else {
+                    const car = redis
+                    res.send(redis);
+                    console.log(`Redis : read cars from redis`);
+                }
+            });
+
+        } else {
+            pgClient.query(`SELECT * FROM cars WHERE id='${id}';`, (err, result) => {
+                if (err) {
+                    res.send("No data found in database")
+                    console.log("Postgres : no data found in database")
+                } else {
+                    const data = result.rows[0];
+                    res.send(data.rows[0]);
+                    console.log(`Postgres : read cars from database`);
+                }
+            });
+        }
+    })
+    
 });
 
-const PORT = 5000;
+const PORT = 8090;
 app.listen(PORT, () => {
     console.log(`API listening on port ${PORT}`);
 });
